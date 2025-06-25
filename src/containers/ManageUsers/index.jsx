544a6bbc35a22e9c09c29c171d2f9dff472f57ba
smforntend <khanabdar038@gmail.com>
2025-06-25 11:43:32 +0500

    Condition Wrong

--- src/containers/ManageUsers/index.jsx
+++ src/containers/ManageUsers/index.jsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 import CustomTabs from "@/components/CustomTabs";
 import Instructor from "./components/Instructor";
 import CustomTable from "@/components/CustomTable";
-import { INS_HEADER, INS_ROWS, STD_HEADER, STD_ROWS } from "@/constants/contents";
+import { INS_HEADER, INS_ROWS } from "@/constants/contents";
 import Students from "./components/Students";
 import AccessRole from "./components/AccessRole";
 
@@ -25,12 +25,7 @@ const ManageUserContainer = () => {
       </div> */}
       <Hero title="User Management" />
       <CustomTabs options={tabs} isChild />
-      if (value = 0) {
       <CustomTable headers={INS_HEADER} rows={INS_ROWS} />
-      }else if (value = 1) {
-      <CustomTable headers={STD_HEADER} rows={STD_ROWS} />
-        
-      }
       {/* <CustomTabs options={tabs1} width='100%' /> */}
       {/* <CustomTabs options={Subtabs} width="60%" /> */}
     </>
